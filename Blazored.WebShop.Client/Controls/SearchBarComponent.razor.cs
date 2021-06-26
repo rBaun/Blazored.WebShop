b22ee8cd2507ce3f1845460172f9728a6a8081e1
rBaun <runeb95@gmail.com>
2021-06-26 20:03:44 +0200

    Added the ViewProductComponent and improved search bar

---
+++ Blazored.WebShop.Client/Controls/SearchBarComponent.razor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazored.WebShop.Client.Controls
+{
+    public partial class SearchBarComponent
+    {
+        public string SearchTerm { get; set; }
+        [Parameter] public EventCallback<string> OnSearch { get; set; }
+
+        private void HandleSearch()
+        {
+            OnSearch.InvokeAsync(SearchTerm);
+        }
+    }
+}
