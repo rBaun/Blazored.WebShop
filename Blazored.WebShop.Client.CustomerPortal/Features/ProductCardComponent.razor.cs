9f6f3fa2b5fd3494477c0012979e9ccc537ff8b6
rBaun <runeb95@gmail.com>
2021-06-30 19:54:54 +0200

    UI CleanUp: Modified ProductItemComponent to be ProductCardComponent and changed the visual display when searching for products

---
+++ Blazored.WebShop.Client.CustomerPortal/Features/ProductCardComponent.razor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blazored.WebShop.Core.Business.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazored.WebShop.Client.CustomerPortal.Features
+{
+    partial class ProductCardComponent
+    {
+        [Parameter]
+        public Product Product { get; set; }
+    }
+}
