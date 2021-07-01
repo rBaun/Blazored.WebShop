ff0334e6cddeb016a63e9802a17d7940538b6cc3
rBaun <runeb95@gmail.com>
2021-07-01 02:47:34 +0200

    Added the OrderRepository file

---
+++ Blazored.WebShop.Application/Plugins/Persistence/IOrderRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Blazored.WebShop.Core.Business.Models;
+
+namespace Blazored.WebShop.Application.Plugins.Persistence
+{
+    public interface IOrderRepository
+    {
+        Order GetOrderById(int orderId);
+        Order GetOrderByUniqueId(string uniqueId);
+        int CreateOrder(Order order);
+        void UpdateOrder(Order order);
+        IEnumerable<Order> GetOrders();
+        IEnumerable<Order> GetOrdersInProgress();
+        IEnumerable<Order> GetOrdersCompleted();
+
+        IEnumerable<OrderLineItem> GetOrderLineItemsByOrderId(int orderId);
+    }
+}
---
+++ Blazored.WebShop.Data/Repositories/OrderRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Blazored.WebShop.Application.Plugins.Persistence;
+using Blazored.WebShop.Core.Business.Models;
+
+namespace Blazored.WebShop.Data.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private Dictionary<int, Order> _orders;
+
+        public OrderRepository()
+        {
+            _orders = new Dictionary<int, Order>();
+        }
+
+        public Order GetOrderById(int orderId)
+        {
+            return _orders[orderId];
+        }
+
+        public Order GetOrderByUniqueId(string uniqueId)
+        {
+            return _orders.SingleOrDefault(order => order.Value.UniqueId == uniqueId).Value;
+        }
+
+        public int CreateOrder(Order order)
+        {
+            order.OrderId = _orders.Count + 1;
+            order.UniqueId = Guid.NewGuid().ToString();
+            _orders.Add(order.OrderId.Value, order);
+
+            return order.OrderId.Value;
+        }
+
+        public void UpdateOrder(Order order)
+        {
+            if (order?.OrderId == null) return;
+            if (_orders[order.OrderId.Value] == null) return;
+
+            _orders[order.OrderId.Value] = order;
+        }
+
+        public IEnumerable<Order> GetOrders()
+        {
+            return _orders.Values;
+        }
+
+        public IEnumerable<Order> GetOrdersInProgress()
+        {
+            var allOrders = (IEnumerable<Order>) _orders.Values;
+
+            return allOrders.Where(order => !order.OrderFinishedOn.HasValue);
+        }
+
+        public IEnumerable<Order> GetOrdersCompleted()
+        {
+            var allOrders = (IEnumerable<Order>) _orders.Values;
+
+            return allOrders.Where(order => order.OrderFinishedOn.HasValue);
+        }
+
+        public IEnumerable<OrderLineItem> GetOrderLineItemsByOrderId(int orderId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
