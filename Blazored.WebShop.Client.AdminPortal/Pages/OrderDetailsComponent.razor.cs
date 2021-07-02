d72b245a2542a0d6888fca6061d65193ccad0f90
rBaun <runeb95@gmail.com>
2021-07-02 15:27:14 +0200

    Adding the Admin's username and secret route

--- Blazored.WebShop.Client.AdminPortal/Pages/OrderDetailsComponent.razor.cs
+++ Blazored.WebShop.Client.AdminPortal/Pages/OrderDetailsComponent.razor.cs
@@ -7,6 +7,7 @@ using Blazored.WebShop.Application.UseCases.AdminPortal.OrderDetails.Interfaces;
 using Blazored.WebShop.Core.Business.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace Blazored.WebShop.Client.AdminPortal.Pages
 {
@@ -14,12 +15,19 @@ namespace Blazored.WebShop.Client.AdminPortal.Pages
     partial class OrderDetailsComponent
     {
         [Inject] public NavigationManager NavigationManager { get; set; }
+        [CascadingParameter] public Task<AuthenticationState> AuthenticationStateTask { get; set; }
 
         [Parameter] public int OrderId { get; set; }
         [Inject] public IViewOrderDetails ViewOrderDetails { get; set; }
         [Inject] public IProcessOrder ProcessOrder { get; set; }
 
         private Order _order;
+        private AuthenticationState _authenticationState;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _authenticationState = await AuthenticationStateTask;
+        }
 
         protected override void OnParametersSet()
         {
@@ -33,10 +41,10 @@ namespace Blazored.WebShop.Client.AdminPortal.Pages
 
         private void HandleProcessOrder()
         {
-            if (_order?.OrderId == null) 
+            if (_order?.OrderId == null && _authenticationState != null) 
                 return;
 
-            ProcessOrder.Execute(_order.OrderId.Value, "admin");
+            ProcessOrder.Execute(_order.OrderId.Value, _authenticationState.User.Identity.Name);
             NavigationManager.NavigateTo("/orders-in-progress");
         }
     }
