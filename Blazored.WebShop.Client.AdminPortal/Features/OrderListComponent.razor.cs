6a57070466751495f9372dcadfbac60d36f896fe
rBaun <runeb95@gmail.com>
2021-07-02 02:20:48 +0200

    Added the ViewOrdersInProgressComponent page

---
+++ Blazored.WebShop.Client.AdminPortal/Features/OrderListComponent.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blazored.WebShop.Core.Business.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazored.WebShop.Client.AdminPortal.Features
+{
+    partial class OrderListComponent
+    {
+        [Parameter] public IEnumerable<Order> Orders { get; set; }
+    }
+}
---
+++ Blazored.WebShop.Client.AdminPortal/Pages/ViewOrdersInProgressComponent.razor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blazored.WebShop.Application.UseCases.AdminPortal.OutstandingOrders.Interfaces;
+using Blazored.WebShop.Core.Business.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazored.WebShop.Client.AdminPortal.Pages
+{
+    partial class ViewOrdersInProgressComponent
+    {
+        [Inject] public IViewOrdersInProgress ViewOrdersInProgress { get; set; }
+        private IEnumerable<Order> _orders;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            _orders = ViewOrdersInProgress.Execute();
+        }
+    }
+}
