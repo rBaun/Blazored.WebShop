8602c9479afdc540c9a53b8b6d4d9aff09a884b5
rBaun <runeb95@gmail.com>
2021-07-02 02:45:19 +0200

    Added the OrdersCompletedComponent page

---
+++ Blazored.WebShop.Client.AdminPortal/Pages/OrdersCompletedComponent.razor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Blazored.WebShop.Application.UseCases.AdminPortal.ProcessedOrders.Interfaces;
+using Blazored.WebShop.Core.Business.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazored.WebShop.Client.AdminPortal.Pages
+{
+    partial class OrdersCompletedComponent
+    {
+        [Inject] public IViewOrdersCompleted ViewOrdersCompleted{ get; set; }
+        private IEnumerable<Order> _orders;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            _orders = ViewOrdersCompleted.Execute();
+        }
+    }
+}
